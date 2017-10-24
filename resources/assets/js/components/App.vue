b247c7fd1a3ac709a00a0329c63f43bb83f2ac8f
John Corry <jcorry@gmail.com>
2017-10-24 06:48:29 -0400

    Remove unnecessary files from git control

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -8,7 +8,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">5TG</a>
+        <a class="navbar-brand" href="#">Brand</a>
       </div>
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
